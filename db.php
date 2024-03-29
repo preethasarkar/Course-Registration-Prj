a828fdf094355d6b8dbae293de05f61876c4b4cd
Reema <reemaacse@gmail.com>
2024-03-30 00:42:16 +0530

    Changes in faculty and student dashboard

--- db.php
+++ db.php
@@ -1,13 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-<<<<<<< Updated upstream
 $password = "preetha";
 $database = "course_registration";
-=======
-$password = "Rmin!03007";
-$database = "course_reg";
->>>>>>> Stashed changes
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);

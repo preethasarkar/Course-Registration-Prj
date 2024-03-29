5e737890f6f31d5368625291324ed15d3ac80a9c
Reema <reemaacse@gmail.com>
2024-03-30 02:14:02 +0530

    Navbar changes

--- student_dashboard.php
+++ student_dashboard.php
@@ -26,15 +26,16 @@
 
     echo ' <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="height:5rem !important;">
         <div class="container-fluid">
+        <a class="navbar-brand" href="#">Student Dashboard</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-        <li class="nav-item">
+        <li class="nav-item" >
             <a class="nav-link active" aria-current="page" href="student_dashboard.php">My Courses</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" >
             <a class="nav-link active" href="register.php" aria-current="page">Register</a>
         </li>
         </ul>
